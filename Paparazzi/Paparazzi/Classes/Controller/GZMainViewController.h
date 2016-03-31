e424cb57d27dc09a1847670e89ef3a4dc4d612ce
jiangtian <jiangtian.ctgu@outlook.com>
2016-03-31 11:13:34 +0800

    初始化项目

---
+++ Paparazzi/Paparazzi/Classes/Controller/GZMainViewController.h
@@ -0,0 +1,13 @@
+//
+//  GZMainViewController.h
+//  Paparazzi
+//
+//  Created by jiangtian on 16-3-31.
+//  Copyright (c) 2016年 jiangtian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GZMainViewController : UIViewController
+
+@end
